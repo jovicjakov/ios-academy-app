a81e1873c649e58f2edc7f07b70c6a78cb5f3ee9
Jakov Jovic <jakov2000@hotmail.com>
2022-07-13 13:27:00 +0200

    Resolved review comments

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -7,27 +7,39 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var button: UIButton!
+    // MARK: - Outlets
     
-    var currentValue: Int = 0 {
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var incrementButton: UIButton!
+    
+    // MARK: - Properties
+    
+    private var currentValue: Int = 0 {
             didSet {
-                counterLabel.text = "\(currentValue)"
+                titleLabel.text = "\(currentValue)"
             }
         }
     
-    @IBAction func tappedButton(_ sender: UIButton) {
-        currentValue += 1
-    }
+    // MARK: - Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        button.layer.cornerRadius = 15
-        button.backgroundColor = UIColor.lightGray
-        
+        setupUI()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func tappedButton() {
+        currentValue += 1
+    }
+    
+    // MARK: - Utility methods
+    
+    private func setupUI() {
+        incrementButton.layer.cornerRadius = 15
+        incrementButton.backgroundColor = UIColor.lightGray
     }
     
 }
